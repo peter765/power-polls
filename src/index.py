87d11b5cc66e3747c00d1beb33f3fdce9b162c8a
Bharathi <bharathimurugan@gmail.com>
2017-10-14 04:59:56 -0400

    delete import statement

--- src/index.py
+++ src/index.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import urllib2
 import requests
 import json
 import time
@@ -13,5 +12,5 @@ def index():
 
   return render_template("index.html")
 
-if __name__ == '__main__': 
-  app.run(host="0.0.0.0", port=8080) 
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=8000)
