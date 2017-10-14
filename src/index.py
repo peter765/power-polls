97ee053915c8bb2abb94c928b87f3373100fad86
Bharathi <bharathimurugan@gmail.com>
2017-10-14 08:09:11 -0400

    Updated first page, added second page
    TODO: Voting, Data Viz

--- src/index.py
+++ src/index.py
@@ -9,8 +9,14 @@ from flask import render_template, redirect, render_template_string
 app = Flask(__name__)
 @app.route("/")
 def index():
+  fakeData = {'one': ['BillOne','2017-05-13','123',456],'two':['BillTwo','2017-03-13','589',654], 'three':['BillThree','2017-02-18','48',13]}
+  return render_template("index.html", data=fakeData)
 
-  return render_template("index.html")
+@app.route("/billview/<string:bill_id>", methods=['GET'])
+def view_bill_details(bill_id):
+  fakeData = {'summary':'summary of infomration will go here','components':['componentid', 'information for compoenent component']}
+  return render_template("summary.html", data=fakeData)
+
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8000)
