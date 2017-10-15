d2bebbbcd2faa08cd1f080e3de6273d75cbe2060
Bharathi <bharathimurugan@gmail.com>
2017-10-14 22:06:06 -0400

    Added several more graphs, cards, etc.

--- src/index.py
+++ src/index.py
@@ -9,14 +9,20 @@ from flask import render_template, redirect, render_template_string
 app = Flask(__name__)
 @app.route("/")
 def index():
+  #title, latest_major_action_date, $id, total_votes
   fakeData = {'one': ['BillOne','2017-05-13','123',456],'two':['BillTwo','2017-03-13','589',654], 'three':['BillThree','2017-02-18','48',13]}
+  url="https://congress-app.azurewebsites.net/api/trending?code=YnQeg28fJHri0iFVl25OF8ggle9Wfc4d1fyAZl22NONQ7E6GtZZADw=="
+  #response = requests.request(method='get', url=url)
+  #response = response.json()
+  #return render_template("index.html", data=json.dumps(response))
   return render_template("index.html", data=fakeData)
 
 @app.route("/billview/<string:bill_id>", methods=['GET'])
 def view_bill_details(bill_id):
+  #summary
   fakeData = {'summary':'summary of infomration will go here','components':['componentid', 'information for compoenent component']}
   return render_template("summary.html", data=fakeData)
 
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=8000)
+  app.run(host="0.0.0.0", port=8050)
