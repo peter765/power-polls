ef37f92cd22c41525f7d841e987622d95fbcd56b
Kairi Kozuma <kairikozuma@gmail.com>
2017-10-15 07:49:28 -0400

    Abbrv to state

--- aggr/state_to_bills.py
+++ aggr/state_to_bills.py
@@ -6,6 +6,68 @@ AWS_ACCESS_KEY='AKIAJCYCZPH7F53LXXHQ'
 AWS_SECRET_KEY='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'
 REGION="us-east-1"
 
+abbrev_to_full = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+}
+
 def get_state_to_bills():
 
     dynamodb = boto3.resource('dynamodb',
@@ -24,7 +86,8 @@ def get_state_to_bills():
     states = {}
     for bill in bills:
         states[bill['sponsor']['state']] = states.get(bill['sponsor']['state'], 0) + 1
-    print(states)
-    return states
 
-get_state_to_bills()
+    return {abbrev_to_full[abv]: count for abv, count in states.items()}
+
+val = get_state_to_bills()
+print(val)
